fec357cb51886848e84d7c96a01366b66ba55a81
BrianJayD <brian.jay1795@gmail.com>
2021-03-14 19:03:51 -0400

    Menu page checks if user is logged in

--- js/menu.js
+++ js/menu.js
@@ -12,6 +12,26 @@ $.ajax({
   }
 });
 
+// Get current user info
+if ("toraunttini_userList" in localStorage) {
+  userList = JSON.parse(localStorage.getItem("toraunttini_userList"));
+}
+
+if ("toraunttini_currentUser" in localStorage) {
+  var currentUserName = localStorage.getItem("toraunttini_currentUser");
+  login(currentUserName);
+}
+
+function login(currentUserName) {
+  console.log("login: " + currentUserName);
+  localStorage.setItem("toraunttini_currentUser", currentUserName);
+  currentUser = userList.find(elem => elem.username === currentUserName);
+
+  document.getElementById("nav-login").innerHTML = "Account";
+  document.getElementById("nav-login").href = "account.html";
+}
+
+// Load items from menu
 function loadMenu(data) {
   const menuList = data;
 
