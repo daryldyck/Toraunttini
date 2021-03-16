d63bf5721fdd4f3b4b5d57e3ea7f9d701d3f0aa4
BrianJayD <brian.jay1795@gmail.com>
2021-03-16 05:52:59 -0400

    Login page now displays cart quantity.

--- js/login.js
+++ js/login.js
@@ -4,18 +4,16 @@ var loginForm = document.getElementById("loginForm");
 var regForm = document.getElementById("regForm");
 var indicator = document.getElementById("login-indicator");
 
-function moveToRegisterForm()
-{
-    regForm.style.transform = "translateX(300px)";
-    loginForm.style.transform = "translateX(300px)";
-    indicator.style.transform = "translateX(0px)";
+function moveToRegisterForm() {
+  regForm.style.transform = "translateX(300px)";
+  loginForm.style.transform = "translateX(300px)";
+  indicator.style.transform = "translateX(0px)";
 }
 
-function moveToLoginForm()
-{
-    regForm.style.transform = "translateX(0px)";
-    loginForm.style.transform = "translateX(0px)";
-    indicator.style.transform = "translateX(100px)";
+function moveToLoginForm() {
+  regForm.style.transform = "translateX(0px)";
+  loginForm.style.transform = "translateX(0px)";
+  indicator.style.transform = "translateX(100px)";
 }
 
 // < !-- ----------- REGISTER/LOGIN ------------ -->
@@ -23,85 +21,88 @@ function moveToLoginForm()
 var userName = document.getElementById("userName");
 var password = document.getElementById("password");
 var confirmPassword = document.getElementById("confirmPassword");
+addCartQuantity();
 
-function validateUserName()
-{
-    console.log("validateUserName");
+function validateUserName() {
+  console.log("validateUserName");
 
-    if (userList.some(elem => elem.username === userName.value.toLowerCase()))
-    {
-        userName.setCustomValidity("Username already exists.");
-    }
-    else
-    {
-        userName.setCustomValidity('');
-    }
+  if (userList.some(elem => elem.username === userName.value.toLowerCase())) {
+    userName.setCustomValidity("Username already exists.");
+  } else {
+    userName.setCustomValidity('');
+  }
 }
 
-function validatePassword()
-{
-    if (password.value != confirmPassword.value)
-    {
-        confirmPassword.setCustomValidity("Passwords Don't Match");
-    } else
-    {
-        confirmPassword.setCustomValidity('');
-    }
+function validatePassword() {
+  if (password.value != confirmPassword.value) {
+    confirmPassword.setCustomValidity("Passwords Don't Match");
+  } else {
+    confirmPassword.setCustomValidity('');
+  }
 }
 
 userName.onchange = validateUserName;
 password.onchange = validatePassword;
 confirmPassword.onkeyup = validatePassword;
 
-function register()
-{
-    var username = document.forms["registerInfo"]["username"].value.toLowerCase();
-    var email = document.forms["registerInfo"]["email"].value.toLowerCase();
-    var password = document.forms["registerInfo"]["password"].value;
-    var firstName = document.forms["registerInfo"]["firstName"].value;
-    var lastName = document.forms["registerInfo"]["lastName"].value;
-    var phone = document.forms["registerInfo"]["phone"].value;
-    var address = document.forms["registerInfo"]["address"].value;
+function register() {
+  var username = document.forms["registerInfo"]["username"].value.toLowerCase();
+  var email = document.forms["registerInfo"]["email"].value.toLowerCase();
+  var password = document.forms["registerInfo"]["password"].value;
+  var firstName = document.forms["registerInfo"]["firstName"].value;
+  var lastName = document.forms["registerInfo"]["lastName"].value;
+  var phone = document.forms["registerInfo"]["phone"].value;
+  var address = document.forms["registerInfo"]["address"].value;
 
-    const newUser = new user(username, email, password, firstName, lastName, phone, address);
-    userList.push(newUser);
-    localStorage.setItem("toraunttini_userList", JSON.stringify(userList));
-    localStorage.setItem("toraunttini_currentUser", username);
+  const newUser = new user(username, email, password, firstName, lastName, phone, address);
+  userList.push(newUser);
+  localStorage.setItem("toraunttini_userList", JSON.stringify(userList));
+  localStorage.setItem("toraunttini_currentUser", username);
 }
 
 var loginUsername = document.getElementById("loginUsername");
 var loginPassword = document.getElementById("loginPassword");
 
-function validateUserExists()
-{
-    console.log("validateUserExists");
+function validateUserExists() {
+  console.log("validateUserExists");
 
-    if (!userList.some(elem => elem.username === loginUsername.value.toLowerCase()))
-    {
-        loginUsername.setCustomValidity("Username does not exist.");
-    }
-    else
-    {
-        loginUsername.setCustomValidity('');
-    }
+  if (!userList.some(elem => elem.username === loginUsername.value.toLowerCase())) {
+    loginUsername.setCustomValidity("Username does not exist.");
+  } else {
+    loginUsername.setCustomValidity('');
+  }
 }
 
-function authorize()
-{
-    console.log("authorize");
-    var username = document.forms["loginInfo"]["loginUsername"].value.toLowerCase();
-    var password = document.forms["loginInfo"]["loginPassword"].value;
+function authorize() {
+  console.log("authorize");
+  var username = document.forms["loginInfo"]["loginUsername"].value.toLowerCase();
+  var password = document.forms["loginInfo"]["loginPassword"].value;
 
-    var loginUser = userList.find(elem => elem.username === username);
+  var loginUser = userList.find(elem => elem.username === username);
 
-    if (loginUser != null && password == loginUser.password)
-    {
-        loginPassword.setCustomValidity('');
+  if (loginUser != null && password == loginUser.password) {
+    loginPassword.setCustomValidity('');
+  } else {
+    loginPassword.setCustomValidity("Incorrect Password.");
+  }
+}
+
+function addCartQuantity() {
+  var qty = 0;
+  if ("cart" in localStorage) {
+    const cart = JSON.parse(localStorage.getItem("cart"));
+    console.log(cart);
+    for (var i = 0; i < cart.length; i++) {
+      console.log("adding " + cart[i].quantity);
+      qty += cart[i].quantity;
     }
-    else
-    {
-        loginPassword.setCustomValidity("Incorrect Password.");
-    }
+  }
+  console.log(qty);
+  if (qty > 0) {
+    $("#navCartQty").text(qty);
+  } else if (qty <= 0) {
+    $("#navCartQty").text("");
+  }
 }
 
 loginUsername.onchange = validateUserExists;
