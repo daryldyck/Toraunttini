e23945c8f57dc821b2ce5523560d3889ac8e951a
BrianJayD <brian.jay1795@gmail.com>
2021-03-16 05:52:22 -0400

    History page now displays cart quantity.

--- js/history.js
+++ js/history.js
@@ -2,6 +2,28 @@ console.log("history.js");
 
 var userList = [];
 
+//Get cart quantity
+//Add cart quantity
+addCartQuantity();
+
+function addCartQuantity() {
+  var qty = 0;
+  if ("cart" in localStorage) {
+    const cart = JSON.parse(localStorage.getItem("cart"));
+    console.log(cart);
+    for (var i = 0; i < cart.length; i++) {
+      console.log("adding " + cart[i].quantity);
+      qty += cart[i].quantity;
+    }
+  }
+  console.log(qty);
+  if (qty > 0) {
+    $("#navCartQty").text(qty);
+  } else if (qty <= 0) {
+    $("#navCartQty").text("");
+  }
+}
+
 // Get current user info
 if ("toraunttini_userList" in localStorage) {
   userList = JSON.parse(localStorage.getItem("toraunttini_userList"));
